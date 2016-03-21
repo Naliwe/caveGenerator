9f68dfa67164a4d16e003a925832a463cd9f6536
Naliwe <r.fourdrilis@gmail.com>
2016-03-21 18:44:43 +0100

    Fixed NullRef

--- Assets/Scripts/CaveGenerator.cs
+++ Assets/Scripts/CaveGenerator.cs
@@ -1,46 +1,32 @@
-<<<<<<< HEAD
 ﻿using System;
 using UnityEngine;
 using System.Collections.Generic;
 
 public class CaveGenerator
 {
-    private int _caveHeight = 0;
+  private int _caveHeight = 0;
 
-    private LevelMap _map;
-    private CellularAutomaton _ca;
-
-    public CaveGenerator( int height, List<Cell> mapBase )
-    {
-        _caveHeight = height;
-        _map = new LevelMap( _caveHeight );
-        _map[0] = mapBase;
-
-        // TODO: create CA
-    }
-
-    void Generate()
-    {
-        for ( int i = 0; i < _caveHeight; i++ )
-        {
-            _map[i] = new List<Cell>( _map[i - 1] );
-            _map[-i] = new List<Cell>( _map[i - 1] );
-
-
-            // TODO: call CA
-        }
-    }
-=======
-﻿using UnityEngine;
-using System.Collections;
-
-public class CaveGenerator
-{
   private LevelMap _map;
+  private CellularAutomaton _ca;
 
-  public CaveGenerator(int[,] middle)
+  public CaveGenerator(int height, List<Cell> mapBase)
   {
-    _map = new LevelMap(middle, 10);
+    _caveHeight = height;
+    _map = new LevelMap(_caveHeight);
+    _map[0] = new List<Cell>(mapBase);
+
+    // TODO: create CA
   }
->>>>>>> 071722b368a0f1b77fb7b1f16d2fa98e964ae1ab
+
+  void Generate()
+  {
+    for (int i = 0; i < _caveHeight; i++)
+    {
+      _map[i] = new List<Cell>(_map[i - 1]);
+      _map[-i] = new List<Cell>(_map[i - 1]);
+
+
+      // TODO: call CA
+    }
+  }
 }
--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -7,146 +7,149 @@ using System.Security.Policy;
 
 public class LevelMap
 {
-    private Dictionary<int, List<Cell>> _map;
+  private Dictionary<int, List<Cell>> _map;
 
-    public int Height { get; set; }
+  public int Height { get; set; }
 
-    public LevelMap( int height )
-    {
-        Height = height;
-    }
+  public LevelMap(int height)
+  {
+    Height = height;
+    _map = new Dictionary<int, List<Cell>>();
+  }
 
-    public LevelMap( int[,] rawMap, int height )
-    {
-        Height = height;
+  public LevelMap(int[,] rawMap, int height)
+  {
+    Height = height;
 
-        List<Cell> tmp = new List<Cell>();
+    List<Cell> tmp = new List<Cell>();
 
-        for ( int x = 0; x < rawMap.GetLength( 0 ); x++ )
-            for ( int y = 0; y < rawMap.GetLength( 1 ); y++ )
-                tmp.Add( new Cell( new Coord( x, y ) ) );
+    for (int x = 0; x < rawMap.GetLength(0); x++)
+      for (int y = 0; y < rawMap.GetLength(1); y++)
+        tmp.Add(new Cell(new Coord(x, y)));
 
-        _map[0] = tmp;
-    }
+    _map[0] = tmp;
+  }
 
-    public List<Cell> this[ int layer ]
-    {
-        get { return _map[layer]; }
-        set { _map[layer] = value; }
-    }
+  public List<Cell> this[int layer]
+  {
+    get { return _map[layer]; }
+    set { _map[layer] = new List<Cell>(value); }
+  }
 }
 
 public struct Coord
 {
-    public int tileX;
-    public int tileY;
+  public int tileX;
+  public int tileY;
 
-    public Coord( int x, int y )
-    {
-        tileX = x;
-        tileY = y;
-    }
+  public Coord(int x, int y)
+  {
+    tileX = x;
+    tileY = y;
+  }
 }
 
 public class Cell : IEquatable<Cell>
 {
-    public Coord Position { get; set; }
-    public int Type { get; set; }
+  public Coord Position { get; set; }
+  public int Type { get; set; }
 
-    public Cell( Coord position )
-    {
-        Position = position;
-    }
+  public Cell(Coord position)
+  {
+    Position = position;
+  }
 
-    public void GrowTo( Cell other )
-    {
-        other.Type = this.Type;
-    }
+  public void GrowTo(Cell other)
+  {
+    other.Type = this.Type;
+  }
 
-    public override bool Equals( object other )
-    {
-        return ( this.Equals( other as Cell ) );
-    }
+  public override bool Equals(object other)
+  {
+    return (this.Equals(other as Cell));
+  }
 
-    public override int GetHashCode()
-    {
-        return ( Position.GetHashCode() * 397 ) ^ Type;
-    }
+  public override int GetHashCode()
+  {
+    return (Position.GetHashCode()*397) ^ Type;
+  }
 
-    public bool Equals( Cell other )
-    {
-        return ( Position.tileX == other.Position.tileX &&
-                 Position.tileY == other.Position.tileY );
-    }
+  public bool Equals(Cell other)
+  {
+    return (Position.tileX == other.Position.tileX &&
+            Position.tileY == other.Position.tileY);
+  }
 }
 
 
 public class Room : IComparable<Room>
 {
-    public List<Coord> tiles;
-    public List<Coord> edgeTiles;
-    public List<Room> connectedRooms;
-    public int roomSize;
-    public bool isAccessibleFromMainRoom;
-    public bool isMainRoom;
+  public List<Coord> tiles;
+  public List<Coord> edgeTiles;
+  public List<Room> connectedRooms;
+  public int roomSize;
+  public bool isAccessibleFromMainRoom;
+  public bool isMainRoom;
 
-    public Room() { }
+  public Room()
+  {
+  }
 
-    public Room( List<Coord> roomTiles, int[,] map )
+  public Room(List<Coord> roomTiles, int[,] map)
+  {
+    tiles = roomTiles;
+    roomSize = tiles.Count;
+    connectedRooms = new List<Room>();
+
+    edgeTiles = new List<Coord>();
+    foreach (Coord tile in tiles)
     {
-        tiles = roomTiles;
-        roomSize = tiles.Count;
-        connectedRooms = new List<Room>();
-
-        edgeTiles = new List<Coord>();
-        foreach ( Coord tile in tiles )
+      for (int x = tile.tileX - 1; x <= tile.tileX + 1; x++)
+      {
+        for (int y = tile.tileY - 1; y <= tile.tileY + 1; y++)
         {
-            for ( int x = tile.tileX - 1; x <= tile.tileX + 1; x++ )
+          if (x == tile.tileX || y == tile.tileY)
+          {
+            if (map[x, y] == 1)
             {
-                for ( int y = tile.tileY - 1; y <= tile.tileY + 1; y++ )
-                {
-                    if ( x == tile.tileX || y == tile.tileY )
-                    {
-                        if ( map[x, y] == 1 )
-                        {
-                            edgeTiles.Add( tile );
-                        }
-                    }
-                }
+              edgeTiles.Add(tile);
             }
+          }
         }
+      }
     }
+  }
 
-    public void SetAccessibleFromMainRoom()
+  public void SetAccessibleFromMainRoom()
+  {
+    if (!isAccessibleFromMainRoom)
     {
-        if ( !isAccessibleFromMainRoom )
-        {
-            isAccessibleFromMainRoom = true;
-            foreach ( Room connectedRoom in connectedRooms )
-            {
-                connectedRoom.SetAccessibleFromMainRoom();
-            }
-        }
+      isAccessibleFromMainRoom = true;
+      foreach (Room connectedRoom in connectedRooms)
+      {
+        connectedRoom.SetAccessibleFromMainRoom();
+      }
     }
+  }
 
-    public static void ConnectRooms( Room roomA, Room roomB )
-    {
-        if ( roomA.isAccessibleFromMainRoom )
-            roomB.SetAccessibleFromMainRoom();
-        else if ( roomB.isAccessibleFromMainRoom )
-            roomA.SetAccessibleFromMainRoom();
+  public static void ConnectRooms(Room roomA, Room roomB)
+  {
+    if (roomA.isAccessibleFromMainRoom)
+      roomB.SetAccessibleFromMainRoom();
+    else if (roomB.isAccessibleFromMainRoom)
+      roomA.SetAccessibleFromMainRoom();
 
-        roomA.connectedRooms.Add( roomB );
-        roomB.connectedRooms.Add( roomA );
-    }
+    roomA.connectedRooms.Add(roomB);
+    roomB.connectedRooms.Add(roomA);
+  }
 
-    public bool IsConnected( Room otherRoom )
-    {
-        return connectedRooms.Contains( otherRoom );
-    }
+  public bool IsConnected(Room otherRoom)
+  {
+    return connectedRooms.Contains(otherRoom);
+  }
 
-    public int CompareTo( Room other )
-    {
-        return ( other.roomSize.CompareTo( roomSize ) );
-    }
+  public int CompareTo(Room other)
+  {
+    return (other.roomSize.CompareTo(roomSize));
+  }
 }
